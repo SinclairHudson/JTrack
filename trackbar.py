728df6476e258435788487a7cd1ee90305e9a3a4
Sinclair Hudson <hudson.sinclair00@gmail.com>
2021-01-16 06:50:41 -0500

    adding trackbar for thresholding

---
+++ trackbar.py
@@ -0,0 +1,56 @@
+import cv2
+import numpy as np
+
+
+def callback(x):
+    pass
+
+
+def hsv_mask(img_path):
+
+    frame = cv2.imread(img_path)
+    cv2.namedWindow('trackbars')
+
+    # inital values
+    ilowH = 16
+    ihighH = 48
+    ilowS = 147
+    ihighS = 253
+    ilowV = 150
+    ihighV = 255
+
+    # create trackbars for color change
+    cv2.createTrackbar('lowH', 'trackbars', ilowH, 180, callback)
+    cv2.createTrackbar('highH', 'trackbars', ihighH, 180, callback)
+
+    cv2.createTrackbar('lowS', 'trackbars', ilowS, 255, callback)
+    cv2.createTrackbar('highS', 'trackbars', ihighS, 255, callback)
+
+    cv2.createTrackbar('lowV', 'trackbars', ilowV, 255, callback)
+    cv2.createTrackbar('highV', 'trackbars', ihighV, 255, callback)
+
+    while True:
+        height, width = frame.shape[:2]
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        ilowH = cv2.getTrackbarPos('lowH', 'trackbars')
+        ihighH = cv2.getTrackbarPos('highH', 'trackbars')
+        ilowS = cv2.getTrackbarPos('lowS', 'trackbars')
+        ihighS = cv2.getTrackbarPos('highS', 'trackbars')
+        ilowV = cv2.getTrackbarPos('lowV', 'trackbars')
+        ihighV = cv2.getTrackbarPos('highV', 'trackbars')
+        lower_hsv = np.array([ilowH, ilowS, ilowV])
+        higher_hsv = np.array([ihighH, ihighS, ihighV])
+        mask = cv2.inRange(hsv, lower_hsv, higher_hsv)
+        dim = (width // 3, height // 3)
+        cv2.imshow('trackbars', cv2.resize(mask, dim, interpolation=cv2.INTER_AREA))
+        cv2.imshow('original image', cv2.resize(frame, dim, interpolation=cv2.INTER_AREA))
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cv2.destroyAllWindows()
+    return lower_hsv, higher_hsv
+
+
+if __name__ == '__main__':
+    hsv_mask("frames/frame000000.jpg")
